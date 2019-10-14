f043214386be6e8eb42568d3a6096185052e5a19
Travis CI User <travis@example.org>
2019-10-14 09:28:59 +0000

    Documentation upload

---
+++ implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["zerocaf"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;Scalar&gt; for <a class=\"struct\" href=\"zerocaf/backend/u64/field/struct.FieldElement.html\" title=\"struct zerocaf::backend::u64::field::FieldElement\">FieldElement</a>",synthetic:false,types:["zerocaf::backend::u64::field::FieldElement"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
